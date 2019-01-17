659663cd9b9af114eef9c134274846b465c42907
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-17 17:38:45 +0300

    adding all things

--- lib/app_three.rb
+++ lib/app_three.rb
@@ -4,8 +4,12 @@ require 'open-uri'
 
 
 # creating all the link inside the nokogiri
-town_hall = Array.new
-page = Nokogiri::HTML(open(­­"https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&lang=fr"))
+town_hall =Array.new
+ page = Nokogiri::HTML(open(­­"https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&lang=fr"))
+
+
+
+
 a = page.xpath("/html/body/div[1]/main/div/div/div[1]/ul[1]/li[1]/h2")
 #prendre les url.txt
 a.each do |url|
